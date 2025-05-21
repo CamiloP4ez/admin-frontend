c71de1c92c8ae0be235eee237be156e70acbb5aa
CamiloP4ez <159430746+CamiloP4ez@users.noreply.github.com>
2025-05-21 22:56:14 +0100

    feat: creating comment Service

---
+++ src/services/commentService.ts
@@ -0,0 +1,23 @@
+// src/services/commentService.ts
+import type { ApiResponseDto } from "../types/api";
+import type { CommentResponseDto } from "../types/comment";
+import apiClient from "./apiClient";
+
+export const getCommentsByPostId = async (
+  postId: string
+): Promise<ApiResponseDto<CommentResponseDto[]>> => {
+  const response = await apiClient.get<ApiResponseDto<CommentResponseDto[]>>(
+    `/v1/posts/${postId}/comments`
+  );
+  if (response.data && Array.isArray(response.data.data)) {
+    response.data.data.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+  }
+  return response.data;
+};
+
+export const deleteComment = async (commentId: string): Promise<void> => {
+  await apiClient.delete(`/v1/comments/${commentId}`);
+};
